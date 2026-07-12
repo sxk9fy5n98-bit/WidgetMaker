746b106af9349f78a66f72bd80fe99e8b22c6514
Jose Ignacio Montivero <j.montivero@icloud.com>
2026-07-12 22:28:00 +1000

    Initial Commit

---
+++ WidgetMaker/WidgetMakerApp.swift
@@ -0,0 +1,17 @@
+//
+//  WidgetMakerApp.swift
+//  WidgetMaker
+//
+//  Created by Jose Ignacio Montivero on 12/7/2026.
+//
+
+import SwiftUI
+
+@main
+struct WidgetMakerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
